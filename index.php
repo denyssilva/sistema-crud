d588e9e7800eff191338a4c07606e7f66896ca0b
Genes Genes <denysvictor8@gmail.com>
2021-09-27 01:12:04 -0300

    metodo post
    os dados serão enviados por método post.

--- index.php
+++ index.php
@@ -8,14 +8,13 @@
 </head>
 <body>
     <h1>Login</h1>
-    <form action="/action_page.php" method="get" target="_blank">
+    <form action="/action_page.php" method="POST">
         <label for="email">Email</label>
         <input type="email" id="email" name="email"><br><br>
 
         <label for="senha">Senha</label>
         <input type="password" id="senha" name="senha"><br><br>
 
-        <input type="submit" value="Submit using GET">
         <input type="submit" formmethod="post" value="Submit using POST">
     </form>
 </body>
