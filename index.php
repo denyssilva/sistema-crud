fb21eb796f26789f5a5c211f5a5fdf1d1c7bd8ae
Genes Genes <91355836+denyssilva@users.noreply.github.com>
2021-09-28 01:34:57 -0300

    mudanças visuais
    melhorei o visual, apenas para ficar mais amigável.

--- index.php
+++ index.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area de Login</title>    
+    <title>Area de Login</title> 
+    <link rel="stylesheet" href="./visual/login.css">   
 </head>
 <body>
-    <h1>Login</h1>
-    <hr>
-    <form action="/action_page.php" method="POST">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email"><br><br>
 
-        <label for="senha">Senha</label>
-        <input type="password" id="senha" name="senha"><br><br>
+    <div class="form">
+        <h1>Login</h1>
+        <hr>
+        <form action="/action_page.php" method="POST">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email"><br><br>
 
-        <input type="submit" formmethod="post" value="Submit using POST">
-    </form>
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha"><br><br>
+
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+
 </body>
 </html>
