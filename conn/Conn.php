7ebbade9f8b2d22a42afbc7f5bc63efb73e59ddb
Genes Genes <91355836+denyssilva@users.noreply.github.com>
2021-09-28 01:53:34 -0300

    Update Conn.php

--- conn/Conn.php
+++ conn/Conn.php
@@ -1,5 +1,20 @@
 <?php
 class Conn {
-    // aqui ha de ser feito a conexao
-    public $instance;
+    public static $instance;
+
+    private function __construct() {
+        //
+    }
+
+    public static function getConexao() {
+        if (!isset(self::$instance)) {
+            // array -> caso tenha erro, lance na excessao e informe qual erro 
+            self::$instance = new PDO('mysql:host=localhost;dbname=mycrud', 'root', '', 
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, ));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        }
+        return self::$instance;
+    }
+    
 }
