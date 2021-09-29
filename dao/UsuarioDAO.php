22c9770cb5d2283966af3972e0b2bd32971f388f
Genes Genes <91355836+denyssilva@users.noreply.github.com>
2021-09-29 02:35:55 -0300

    commit da pagina de teste

--- dao/UsuarioDAO.php
+++ dao/UsuarioDAO.php
@@ -5,14 +5,20 @@ require_once '../conn/Conn.php';
 
 class UsuarioDAO{
     public static function inserir(Usuario $usuario){
-        $sql = "INSERT INTO usuario(nome,email,senha) VALUES (:nome,:email,:senha)";
+        try {
+            $sql = "INSERT INTO usuario(nome,email,senha) VALUES (:nome,:email,:senha)";
         
-        $p_sql = Conn::getConexao()->prepare($sql);
+            $p_sql = Conn::getConexao()->prepare($sql);
 
-        $p_sql->bindvalue(":nome", $usuario->getNome());
-        $p_sql->bindvalue(":email", $usuario->getEmail());
-        $p_sql->bindvalue(":senha", $usuario->getSenha());
+            $p_sql->bindvalue(":nome", $usuario->getNome());
+            $p_sql->bindvalue(":email", $usuario->getEmail());
+            $p_sql->bindvalue(":senha", $usuario->getSenha());
 
-        return $p_sql->execute();
+            return $p_sql->execute();
+
+        } catch (Exception $e) {
+            print "Erro!!!: Nao foi possivel inserir usuario. <br>" . $e . '<br>';
+        }
+        
     }
 }
--- dao/teste.php
+++ dao/teste.php
@@ -3,9 +3,17 @@
 require_once "../model/Usuario.php";
 require_once "../dao/UsuarioDAO.php";
 
+echo "Tela de teste";
+
+
+
+/*
+inserção
+
 $u = new Usuario();
-$u->setNome("Joao");
-$u->setEmail("joao@mail.com");
+$u->setNome("maria");
+$u->setEmail("maria@mail.com");
 $u->setSenha("123");
 
 UsuarioDAO::inserir($u);
+*/
