ab4505150701e076c93f684d9ed3e4b6aa00799d
Genes Genes <91355836+denyssilva@users.noreply.github.com>
2021-10-01 01:26:54 -0300

    mudança no html

--- home.php
+++ home.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area Do Crud</title> 
-    <link rel="stylesheet" href="./visual/login.css">   
-</head>
-<body>
-
 <?php
+include_once "./visual/cab.php";
 
 include_once "./dao/UsuarioDAO.php";
 include_once "./model/Usuario.php";
--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area de Login</title> 
-    <link rel="stylesheet" href="./visual/login.css">   
-</head>
-<body>
+<?php
+include_once "./visual/cab.php";
+?>
 
     <div class="form">
         <h1>Login</h1>
